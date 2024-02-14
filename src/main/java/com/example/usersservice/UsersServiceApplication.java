551c9cbb9b3746a5eff3f90a895fec5efa8bf630
Nageeb Qaid <ng.qaid@gmail.com>
2024-02-14 12:27:31 +0300

    add workflow

--- src/main/java/com/example/usersservice/UsersServiceApplication.java
+++ src/main/java/com/example/usersservice/UsersServiceApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UsersServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UsersServiceApplication.class, args);
+
+			SpringApplication.run(UsersServiceApplication.class, args);
 	}
 
 }
